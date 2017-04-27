e4d34f73485146e58b2db7707872ff505dce95e0
Vit Zdrubecky <vit.zdrubecky@gmail.com>
2017-04-27 19:24:25 +0200

    Starting 18

---
+++ app/src/main/java/cz/zdrubecky/beatbox/BeatBoxActivity.java
@@ -0,0 +1,10 @@
+package cz.zdrubecky.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
